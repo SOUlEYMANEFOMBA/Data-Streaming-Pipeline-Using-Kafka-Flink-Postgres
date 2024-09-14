a91f8d56a4a866881996674f70f0addb6958cfb4
SOUlEYMANEFOMBA <souleymanefmb@gmail.com>
2024-09-14 02:04:09 +0200

    feat: refactor de code

--- Dags/tasks/stream_data.py
+++ Dags/tasks/stream_data.py
@@ -1,13 +1,18 @@
+from kafka import KafkaProducer
 import json
-import requests
 
 class StreamingDataTask():
-    '''Cette task sert à importer notre base de donnée en l'utilisant l' api user random ui'''
-    def __init__(self,url):
-            self.url = url
-            
-    def streaming_data(self):
-       res = requests.get(self.url)
-       respond=res.json()
-       result=respond['results'][0]
-       print(json.dumps(result,indent=5))
+    """Cette task envoie des données formatées à Kafka"""
+    def streaming_data(self, format_data):
+        producer=None
+        try:
+            producer = KafkaProducer(bootstrap_servers='localhost:9092', max_block_ms=5000)
+            producer.send('users_created', json.dumps(format_data).encode('utf-8'))
+            producer.flush()  # Pour s'assurer que le message est bien envoyé avant de fermer le producteur
+            print("Data sent successfully")
+        except Exception as e:
+          print(f"Erreur lors de la production de données Kafka: {e}")
+    
+        finally:
+          if producer is not None:
+            producer.close()
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from Dags.tasks.stream_data import StreamingDataTask
 from Dags.tasks.doawload_data import DoawloadDataTask
 from Dags.tasks.format_data import FormatDataTask
+from Dags.tasks.stream_data import StreamingDataTask
+
 def main():
     doawloaddata = DoawloadDataTask('https://randomuser.me/api/')
     res=doawloaddata.get_data()
     formatData =FormatDataTask()
     result =formatData.format_data(res)
     print(result)
-    
+    stream=StreamingDataTask()
+    stream.streaming_data(result)
 
 if __name__=="__main__":
     main()
